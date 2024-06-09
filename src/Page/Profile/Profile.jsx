455629127746428a85e60e3559e5d5ebc0193dea
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-06-09 09:19:21 +0600

    worked on profile

--- src/Page/Profile/Profile.jsx
+++ src/Page/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 import { FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom";
@@ -6,17 +6,36 @@ import ProfileUpdateModal from "./ProfileUpdateModal";
 import Loading from "../../components/Loading";
 
 const Profile = () => {
-  const { user,  } = useContext(AuthContext);
+  const { user, getBudgetData, getSpendingData } = useContext(AuthContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [spendingDataArray, setSpendingDataArray] = useState([]);
+  const [totalSpending, setTotalSpending] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Ensure getSpendingData is an array
+    if (Array.isArray(getSpendingData)) {
+      setSpendingDataArray(getSpendingData);
+      setIsLoading(false); // Set loading to false when data is fetched
+    }
+  }, [getSpendingData]);
+
+  useEffect(() => {
+    // Calculate total spending
+    const total = spendingDataArray.reduce((sum, item) => sum + item.spendingAmount, 0);
+    setTotalSpending(total);
+  }, [spendingDataArray]);
+
+  const { totalBudget } = getBudgetData;
 
   if (!user) {
-    return <div><Loading/></div>;
+    return <div><Loading /></div>;
   }
 
   const { name, email, photoUrl, address, phone, website } = user;
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto flex flex-col justify-center items-center px-4 py-8">
       <div className="w-10/12 border mx-auto bg-white rounded-xl shadow-md overflow-hidden">
         <div className="flex justify-between space-x-5">
           <div className="flex space-x-5">
@@ -54,6 +73,20 @@ const Profile = () => {
           onClose={() => setIsModalOpen(false)}
         />
       )}
+      <div className="card bg-slate-100 w-1/2 mt-10 border py-5 ">
+        <h1 className="text-center">Your Tour Budget: <span className="font-bold">{totalBudget} Taka</span></h1>
+        
+      </div>
+
+      <div className="card bg-slate-100 w-1/2 mt-4 border py-5 ">
+        <h1 className="text-center">Your Total Spending: <span className="font-bold">{totalSpending} Taka</span></h1>
+        {totalSpending > totalBudget && (
+          <div className="text-center text-red-500 mt-4">
+            You are crossing your budget. Keep concern on spending.
+          </div>
+        )}
+      </div>
+
     </div>
   );
 };
